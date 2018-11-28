1398dc5e24e80cbcf8e200c9b27813e37992c034
SaadeTrabelsi <tinasaade10@gmail.com>
2018-11-28 12:05:01 +0100

    creation de deux methodes : public void createRepresentation() : alloue la matrice représentant l’image. — public void setPixelColor(Color color, int x, int y) : fixe la couleur d’un pixel.

---
+++ src/main/java/BruteRasterImage.java
@@ -0,0 +1,58 @@
+import image.Image;
+import javafx.scene.paint.Color;
+import util.Matrices;
+
+import static util.Matrices.requiresNonNull;
+
+public class BruteRasterImage implements Image {
+
+    public  final int width ;
+    public final int height ;
+    public Color[][] colors ;
+
+
+    public BruteRasterImage(Color color, int width, int height) {
+
+        this.width = width ;
+        this.height = height ;
+        colors = new Color[width][height];
+
+        for(int x=0;x<width;x++) {
+            for(int y=0;y<height;y++) {
+                colors[x][y]=color ;
+            }
+        }
+
+    }
+    public BruteRasterImage(Color[][] colors) {
+        requiresNonNull(colors) ;
+        this.colors=colors.clone() ;
+        this.height= Matrices.getColumnCount(colors);
+        this.width =  Matrices.getRowCount(colors);
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return colors[x][y];
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    public void createRepresentation() {
+        colors = new Color[width][height];
+
+
+    }
+    public void setPixelColor(Color color, int x, int y) {
+         colors[x][y] = color ;
+    }
+
+}
